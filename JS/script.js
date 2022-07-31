8b849a2704297d25cad290769cce1fbafebb4df8
nat <kroshechka.baby@gmail.com>
2022-07-31 10:39:56 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+'use strict';
+// alert('Hello');
+// console.log(1);
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18 лет', '10');
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Как Ваше имя?', '');
+//answers[1] = prompt('Как Ваша фамилия?', '');
+//answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(typeof(answers));
+//console.log(typeof(null));
+
+//const category = 'Toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Nat';
+
+alert(`Привет, ${user}`);
